58e5b5b518958c368387d872ce2c93df058ab9af
vali-snow <vali-snow@users.noreply.github.com>
2019-02-20 23:21:08 +0200

    commit

---
+++ Tema1/Methods.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tema1
+{
+    public class Methods
+    {
+        public static double GetPrecizie()
+        {
+            var precizie = 1.0;
+            while (1.0 + precizie != 1.0)
+            {
+                precizie /= 10;
+            }
+            return precizie;
+        }
+
+        public static bool CheckAdunareAsociativa(double x, double y, double z)
+        {
+            return (x + y) + z == x + (y + z);
+        }
+
+        public static bool CheckInmultireAsociativa(double x, double y, double z)
+        {
+            return (x * y) * z == x * (y * z);
+        }
+
+        public static double CalculatePolinom(int i, double x)
+        {
+            double c1, c2, c3, c4, c5, c6, a1, a2;
+            double xx = x * x;
+            switch (i)
+            {
+                case 1:
+                    c1 = Double.Parse("0.16666666666666666666666666666667");
+                    c2 = Double.Parse("0.00833333333333333333333333333333");
+                    return x * (1 + xx * (-c1 + c2 * xx));
+                case 2:
+                    c1 = Double.Parse("0.16666666666666666666666666666667");
+                    c2 = Double.Parse("0.00833333333333333333333333333333");
+                    c3 = Double.Parse("1.984126984126984126984126984127E-4");
+                    return x * (1 + xx * (-c1 + xx * (c2 - c3 * xx)));
+                case 3:
+                    c1 = Double.Parse("0.16666666666666666666666666666667");
+                    c2 = Double.Parse("0.00833333333333333333333333333333");
+                    c3 = Double.Parse("1.984126984126984126984126984127E-4");
+                    c4 = Double.Parse("2.7557319223985890652557319223986E-6");
+                    return 
+                case 4:
+                    a1 = 0.166;
+                    a2 = 0.00833;
+                    c3 = Double.Parse("1.984126984126984126984126984127E-4");
+                    c4 = Double.Parse("2.7557319223985890652557319223986E-6");
+                    break;
+                case 5:
+                    c1 = Double.Parse("0.16666666666666666666666666666667");
+                    c2 = Double.Parse("0.00833333333333333333333333333333");
+                    c3 = Double.Parse("1.984126984126984126984126984127E-4");
+                    c4 = Double.Parse("2.7557319223985890652557319223986E-6");
+                    c5 = Double.Parse("2.5052108385441718775052108385442E-8");
+                    break;
+                case 6:
+                    c1 = Double.Parse("0.16666666666666666666666666666667");
+                    c2 = Double.Parse("0.00833333333333333333333333333333");
+                    c3 = Double.Parse("1.984126984126984126984126984127E-4");
+                    c4 = Double.Parse("2.7557319223985890652557319223986E-6");
+                    c5 = Double.Parse("2.5052108385441718775052108385442E-8");
+                    c6 = Double.Parse("1.6059043836821614599392377170155E-10");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- Tema1/Program.cs
+++ Tema1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+//using System.Math;
 
 namespace Tema1
 {
@@ -6,7 +7,18 @@ namespace Tema1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /// 1.Precizia Masina
+            var precizieMasina = Methods.GetPrecizie();
+            Console.WriteLine($"Precizia masina pentru limbajul C# este: {precizieMasina}");
+
+            /// 2.Neasociativitate a operatiei de adunare efectuate de calculator cu y,z = precizia masina
+            Console.WriteLine($"Operatia de adunare mai este asociativa cand y si z sunt egale cu precizia masina? {(Methods.CheckAdunareAsociativa(1.0,precizieMasina,precizieMasina)?"Adevarat":"Fals")}");
+
+            /// 2.Neasociativiate a operatiei de inmultire efectuate de calculator - verificare exemplu
+            var (x, y, z) = (1+Double.Parse("1E-15"), Double.Parse("1.0E-10"), Double.Parse("1.0E-10"));
+            Console.WriteLine($"Daca x= {x}, y = {y}, z= {z}, operatia de inmultire mai este asociativa? {(Methods.CheckInmultireAsociativa(x,y,z)?"Adevarat":"Fals")}");
+
+            /// 3.Aproximări polinomiale ale funcţiei sin
         }
     }
 }
